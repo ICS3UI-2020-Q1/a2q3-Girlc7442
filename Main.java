f977a6a3f802a649db7c4826e86cb60c3c64d734
Girlc7442 <girlc7442@wrdsb.ca>
2020-09-15 14:25:43 +0000

    finished with comments

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner;
+
 /**
- *
- * @author 
+ *Puts two integers in ascending order
+ * @author Cole G.
  */
 public class Main {
 
@@ -9,7 +11,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Create a scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    //ask the user for an integer
+    System.out.println("Please enter an integer:");
+    //Get the integer from the user
+    int userNumber1 = input.nextInt();
     
+    //ask the user for another integer
+    System.out.println("Please enter another integer:");
+    //Get the second integer from the user
+    int userNumber2 = input.nextInt();
+
+    //Check to see which number is larger 
+    if(userNumber1 >= userNumber2){
+      //if the first number is larger than the second
+      System.out.println("Your numbers in ascending order are " + userNumber2 + ", " + userNumber1);
+    }else{
+      //if the second number is larger than the first
+      System.out.println("Your numbers in ascending order are " + userNumber1 + ", " + userNumber2);
+    }
   }
 }
